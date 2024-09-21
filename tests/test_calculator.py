71338e240e5bf8542c019e3119ddaa871b053dad
ManaswiniGande28 <167720605+ManaswiniGande28@users.noreply.github.com>
2024-09-20 20:49:52 -0700

    Update test_calculator.py
    Added missing bracket

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,7 +6,7 @@ class TestCalculator(unittest.TestCase):
     self.assertEqual(add(-1, 1), 0)
     self.assertEqual(add(-1, -1), -2)
 def test_subtract(self):
-    self.assertEqual(subtract8, 3), 5)
+    self.assertEqual(subtract(8, 3), 5)
     self.assertEqual(subtract(1, 1), 0)
     self.assertEqual(subtract(-3, -1), -2)
 def test_divide(self):
